d0ea8d3c396869ef90b1839aabf7d104a37f45a2
Rob Jenkins <rwjenkins@gmail.com>
2023-01-11 15:08:29 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const feedData = [
+    { id: 1, text: 'This is post number 1', videoLink: '' },
+    { id: 2, text: 'This is post number 2', videoLink: '' },
+    { id: 3, text: 'This is post number 3', videoLink: '' },
+    { id: 4, text: 'This is post number 4', videoLink: '' },
+    { id: 5, text: 'This is post number 5', videoLink: '' },
+    { id: 6, text: 'This is post number 6', videoLink: '' },
+    { id: 7, text: 'This is post number 7', videoLink: '' },
+    { id: 8, text: 'This is post number 8', videoLink: '' },
+  ];
+
+
+  const renderItem = ({item, index}) => {
+    return (
+      <View style={styles.postView}>
+        <Text>{item.text}</Text>
+      </View>
+    );
+
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+
+      <FlatList
+        data={feedData}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      /> 
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,6 +43,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+
+  postView: {
+    width: '100%',
+    padding: 2,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    borderColor: 'red',
+    borderWidth: 1,
+    borderStyle: 'solid'
+  }
 });
